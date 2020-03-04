15969b0ca90af89803f183d35aa56ebdc4a14578
Denis, Makarov <makarovdenis07081985@gmail.com>
2020-03-04 14:01:28 +0200

    first slide

--- src/js/main.js
+++ src/js/main.js
@@ -1,37 +1,96 @@
 let navHeaderBtn = Array.from(document.getElementsByClassName("nav_head"));
 let navNumbersBtn = Array.from(document.getElementsByClassName("head_p"));
-let countSlide = navHeaderBtn.length;
+let sectionBoxes = Array.from(document.getElementsByClassName("section_box"));
+let countSlide = 0;
 
 
 for (let i = 0; i < navHeaderBtn.length; i++) {
     navHeaderBtn[i].setAttribute("data-number_btn", i);
-    navHeaderBtn[i].style.animationDelay = i/15+"s";
-
-    navNumbersBtn[i].style.animationDelay = i/10+0.5+"s"
+    sectionBoxes[i].setAttribute("data-number_section", i);
+    navHeaderBtn[i].style.animationDelay = i / 15 + "s";
+    navNumbersBtn[i].style.animationDelay = i / 10 + 0.5 + "s"
 }
 
 
-// setTimeout(function () {
-//     navHeaderBtn[0].classList.add("activeEl");
-// },500);
+setTimeout(function () {
+    sectionBoxes[0].classList.add("section_active");
+}, 300);
+setTimeout(function () {
+    navHeaderBtn[0].classList.add("activeEl");
+}, 1000);
 
-navHeaderBtn.map((el) => {
-    el.addEventListener("mouseover", function () {
-        for (let i = 0; i < navHeaderBtn.length; i++) {
-            navHeaderBtn[i].classList.remove("activeEl")
-        }
-        el.classList.add("activeEl");
+
+////  game cards  ////
+function cards_item_first(countSlide) {
+    let cards = Array.from(document.getElementsByClassName("card"));
+    let countDelay = 1.1;
+
+    if (countSlide == 0){
+        cards.map((el)=>{
+            el.style.transitionDelay = countDelay+"s";
+            countDelay = countDelay - 0.1;
+            // el.classList.add("deactiveCards");
+            el.classList.add("activeCards");
+            el.style.bottom = -15+"px";
+
+            el.addEventListener("mouseover", function () {
+                el.style.transitionDelay = 0+"s";
+                el.style.bottom = 15+"px"
+            });
+            el.addEventListener("mouseout", function () {
+                el.style.transitionDelay = 0+"s";
+                el.style.bottom = -15+"px"
+            })
+        })
+    } else if (countSlide != 0){
+        cards.map((el)=>{
+            el.style.transitionDelay = 0+"s";
+            el.classList.remove("activeCards");
+            el.classList.add("deactiveCards");
+            el.style.bottom = 170+"px"
+
+        })
+    }
+
+
+}
+
+cards_item_first(countSlide);
+
+
+function activeElements() {
+    for (let i = 0; i < navHeaderBtn.length; i++) {
+        navHeaderBtn[i].classList.remove("activeEl");
+        sectionBoxes[i].classList.remove("section_active");
+        navHeaderBtn[countSlide].classList.add("activeEl");
+        sectionBoxes[countSlide].classList.add("section_active");
+    }
+}
+
+
+function changeSlideOnClick() {
+    navHeaderBtn.map((el) => {
+        el.addEventListener("mouseover", function () {
+            countSlide = +el.dataset.number_btn;
+            activeElements();
+            cards_item_first(countSlide)
+        });
     });
-});
+}
 
-document.onwheel = function (e) {
-    let speed = e.deltaY;
-    (speed > 0) ? countSlide = countSlide + 1 : countSlide = countSlide;
-    (speed < 0) ? countSlide = countSlide - 1 : countSlide = countSlide;
-    (countSlide == navHeaderBtn.length + 1) ? countSlide = 0 : countSlide = countSlide;
-    (countSlide == -1) ? countSlide = navHeaderBtn.length : countSlide = countSlide;
-    navHeaderBtn.map((el)=>{
-        // el.classList.remove("activeEl");
-        (el.dataset.number_btn == countSlide) ? el.classList.add("activeEl") : el.classList.remove("activeEl")
-    })
-};
+
+function chahgeOnWheel() {
+    document.onwheel = function (e) {
+        let speed = e.deltaY;
+        (speed > 0) ? countSlide = countSlide + 1 : countSlide = countSlide;
+        (speed < 0) ? countSlide = countSlide - 1 : countSlide = countSlide;
+        (countSlide >= navHeaderBtn.length) ? countSlide = 0 : countSlide = countSlide;
+        (countSlide == -1) ? countSlide = navHeaderBtn.length - 1 : countSlide = countSlide;
+        activeElements();
+        cards_item_first(countSlide);
+    };
+}
+
+changeSlideOnClick();
+chahgeOnWheel();
+
